9dd885c0baaef3c47fbae16122588a4c04b21465
tsh <dr.tallin@gmail.com>
2018-05-08 13:00:43 +0300

    complete quicksort

--- 3_quick_sort.py
+++ 3_quick_sort.py
@@ -1,20 +1,30 @@
 """Implement quick sort in Python.
 Input a list.
 Output a sorted list."""
+
+def partition(array, low, high):
+    pivot = array[high]
+    while low < high:
+        if array[low] >= pivot:
+            tmp = array[low]
+            array[low] = array[high-1]
+            array[high-1] = pivot
+            array[high] = tmp
+            high = high - 1
+        else:
+            low += 1
+    return high
+
+def _quicksort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        _quicksort(array, low, pi - 1)
+        _quicksort(array, low + 1, high)
+
 def quicksort(array):
     lo, hi = 0, len(array) - 1
-    print(lo, hi, array)
-    pivot = array[hi]
-    while lo < hi:
-        if array[lo] >= pivot:
-            tmp = array[lo]
-            array[lo] = array[hi-1]
-            array[hi-1] = pivot
-            array[hi] = tmp
-            hi = hi - 1
-        else:
-            lo += 1
-    return quicksort(array[:hi]) + quicksort(array[hi:])
+    _quicksort(array, lo, hi)
+    return array
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
 print quicksort(test)
